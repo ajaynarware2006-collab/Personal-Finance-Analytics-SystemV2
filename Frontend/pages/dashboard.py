ba2daee4e30c8e52a440128f09c5dfd6eb6c667e
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-08 00:23:00 +0530

    Adding Dashborad to the app

---
+++ Frontend/pages/dashboard.py
@@ -0,0 +1,99 @@
+from backend.income import view_income
+from backend.analytics import current_month_expense,top_category,highest_expense,lowest_expense,total_savings,budget_remaining,monthly_savings,monthly_expense,yearly_expense,average_daily_expense,category_summary
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="dashborad",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+st.markdown("""
+<style>
+[data-testid="stSidebar"] {
+    display: none;
+}
+
+[data-testid="stSidebarCollapsedControl"] {
+    display: none;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("<h1 style='background: linear-gradient(to right, #08203E, #557C93); -webkit-background-clip: text; background-clip: text; color: transparent;text-align:center;padding-top:-20px;padding-bottom:40px;'>PERSONAL FINANCE DASHBOARD</h1>",unsafe_allow_html=True)
+
+col1,col2,col3,col4=st.columns([1,1,1,1],gap="large")
+
+with col1:
+    st.metric(label="💰 Income",value=f"₹{view_income(2)}")
+
+with col2:
+
+    st.metric(label="🏆 Top Category",value=f"₹{top_category(2)}")
+
+with col3:
+    st.metric(label="📈 Highest Expense",value=f"₹{highest_expense(2)}")
+
+
+with col4:
+    st.metric(label="📉 Lowest Expense",value=f"₹{lowest_expense(2)}")
+
+
+st.markdown("<BR><BR><BR><BR><BR>",unsafe_allow_html=True)
+col5,col6=st.columns([1,3])
+
+with col5:
+    st.metric("💵TOTAL SAVING YOU HAVE",value=f"₹{total_savings(2)}")
+    st.markdown("<BR><BR><BR>",unsafe_allow_html=True)
+    st.metric("🎯BUDGET REMAINING",value=f"₹{budget_remaining(2)}")
+
+with col6:
+    df=pd.DataFrame(category_summary(2))
+    fig=px.pie(df,names=0,values=2)
+    st.plotly_chart(fig)
+
+
+col7,col8=st.columns(2)
+
+with col7:
+    monthly_exp=monthly_expense(2)
+    if not monthly_exp:
+        st.warning("No data to display yet.")
+    else:
+        df=pd.DataFrame(monthly_exp)
+        df.columns=["Month","Expense"]
+        df["Expense"] = df["Expense"].astype(float)
+        st.line_chart(df,x="Month",y="Expense")
+
+
+with col8:
+    yearly_exp=yearly_expense(2)
+    if not yearly_exp:
+        st.warning("No data to display yet.")
+    else:
+        df=pd.DataFrame(yearly_exp)
+        df.columns=["Month","Expense"]
+        df["Expense"] = df["Expense"].astype(float)
+        st.bar_chart(df,x="Month",y="Expense")
+
+
+avg=average_daily_expense(2)
+if not avg:
+    st.warning("No data to display yet.")
+else:
+    df=pd.DataFrame(avg)
+    df.columns=["Month","Expense"]
+    df["Expense"] = df["Expense"].astype(float)
+    st.bar_chart(df,x="Month",y="Expense")
+
+data=monthly_savings(2)
+if not data:
+    st.warning("No data to display yet.")
+
+else:
+    df=pd.DataFrame(data)
+    df.columns=["Expense","Months"]
+    # df["Expense"] = df["Expense"].astype(float)
+    st.line_chart(df,x="Months",y="Expense")
