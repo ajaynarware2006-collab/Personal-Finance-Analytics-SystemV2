234792ea5ba5ee89a7a63e8b63e9451b6331e612
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-07-10 00:42:44 +0530

    Impove category operations file

--- backend/category_operations.py
+++ backend/category_operations.py
@@ -5,7 +5,7 @@ from backend.connection import connecting
 def view_categories():
     with connecting() as connection:
         with connection.cursor() as cursor:
-            cursor.execute("SELECT category_name FROM category ORDER BY category_id;")
+            cursor.execute("SELECT category_name,category_id FROM category ORDER BY category_id;")
             data=cursor.fetchall()
 
-            return [category[0] for category in data]
+            return [(category[0] , category[1]) for category in data]
